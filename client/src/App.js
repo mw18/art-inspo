a8ad98dc2f8d1e4c54611c5073ae947ad6fa08e0
mw18 <67554339+mw18@users.noreply.github.com>
2021-07-27 13:53:40 -0500

    connect art institute api and retutn results for title, image, description, artist

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import Welcome from '../src/components/Welcome';
+import Welcome from '../src/components/Welcome'
+import useFetch from "react-fetch-hook";
+import React from "react";
 
-function App() {
+
+
+export default function App() {
+  const { isLoading, error, data } = useFetch("https://api.artic.edu/api/v1/artworks/129884");
+
+  if (isLoading) return "Loading...";
+  if (error) return "Error!";
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <Welcome/>
-      </header>
-    </div>
+    <>
+    <Welcome/>
+    
+      <p>{data.data.title}</p>
+      <img src={`https://www.artic.edu/iiif/2/${data.data.image_id}/full/450,/0/default.jpg`}/>
+      <p>Description: {data.data.thumbnail.alt_text}</p>
+      <p>Artist Title: {data.data.artist_title}</p>
+      
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+    </>
   );
 }
-
-export default App;
